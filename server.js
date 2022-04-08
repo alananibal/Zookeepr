ce8a672f80544bf33b0d46aa489f27fa0226f344
AlanAnibalDSR <alananibaldsr@gmail.com>
2022-04-07 17:52:29 -0700

    11.1

--- server.js
+++ server.js
@@ -44,13 +44,23 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
   });
+
+    app.get('/api/animals', (req, res) => {
+        let results = animals;
+        if (req.query) {
+          results = filterByQuery(req.query, results);
+        }
+        res.json(results);
+      });
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
